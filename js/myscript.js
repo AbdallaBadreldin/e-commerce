e2e90bb40a346e28b210816ac096944c209e8c74
Abdalla badr <Bossmail28@gmail.com>
2023-05-07 10:36:58 +0300

    added items without css

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,16 @@
+fetchData();
 function fetchData() {
-  let output = document.getElementById("output");
-  fetch("https://dummyjson.com/products/1")
-    .then((response) => response.json())
+  let output = document.getElementById("products_list");
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
     .then((data) => {
-      output.innerHTML += "id = " + data.id + "<br/>";
-      output.innerHTML += "brand = " + data.brand + "<br/>";
-      output.innerHTML += "category = " + data.category + "<br/>";
-      output.innerHTML += "price = " + data.price + "<br/>";
-      output.innerHTML += "rating = " + data.rating + "<br/>";
-      output.innerHTML += "stock = " + data.stock + "<br/>";
-    });
+      // output.innerHTML += "id = " + data.id + "<br/>";
+      // output.innerHTML += "brand = " + data.brand + "<br/>";
+      // output.innerHTML += "category = " + data.category + "<br/>";
+      // output.innerHTML += "price = " + data.price + "<br/>";
+      // output.innerHTML += "rating = " + data.rating + "<br/>";
+      // output.innerHTML += "stock = " + data.stock + "<br/>";
+      console.log(data);
+    }
+    );
 }
