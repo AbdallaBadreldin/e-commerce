070e8754846322311bfcdbe2ae342537527fafc3
Abdalla badr <Bossmail28@gmail.com>
2023-05-07 12:32:06 +0300

    created page for all profucts

---
+++ js/allProducts.js
@@ -0,0 +1,35 @@
+fetchData();
+function fetchData() {
+  let output = document.getElementsByClassName("products_list");
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+        data.products.forEach((currentElement) => {
+        let mainDiv  =document.createElement('div');
+        let divText = document.createElement('div');
+        let productName = document.createElement('p');
+        productName.className = "name";
+        let price = document.createElement('p');
+        price.className = "price";
+        let divParagrapgh = document.createElement('p')
+        let img = document.createElement('img');
+
+        productName.innerText = currentElement.title;
+        price.innerText = currentElement.price;
+
+        divParagrapgh.innerText = currentElement.description;
+        img.src = currentElement.images[0] ;
+        divText.appendChild(productName);
+        divText.appendChild(price);
+
+        mainDiv.appendChild(img);
+        mainDiv.appendChild(divText);
+        mainDiv.appendChild(divParagrapgh);
+    
+        let output = document.getElementById("products_list");
+       output.appendChild(mainDiv);
+       
+      });
+    
+    });
+}
